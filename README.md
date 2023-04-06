f38615fc3249532811250b8f71fcddfa8d5bdd03
duongnd <duong.tnhp8386@gmail.com>
2023-04-06 15:30:26 +0700

    add: Add hardhat inviroment

---
+++ README.md
@@ -0,0 +1,16 @@
+# hardhat-note
+
+Age is a question of mind over matter. If you don’t mind, it don’t matter.
+
+## Configure Local blockchain
+
+To open port:
+>npx hardhat node --hostname 0.0.0.0 --port 8545
+
+Add positionalPrams to access localhost from hardhat console.
+>npx hardhat console --network localhost.
+
+Add new network in metamask.
+
+
+gasPrice = min {Max fee, base fee + priority}
---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  /**
+   * In order to estimate gas when MetaMask connects to the Hardhat network, it is necessary to set @initialBaseFeePerGas equal to 0.
+   */
+  networks: {
+    hardhat:{
+      accounts:{
+        mnemonic: process.env.MNEMONIC,
+        count: 10,
+        accountsBalance: "100000000000000000000"
+      },
+      // gasPrice: 5*1e9,
+      initialBaseFeePerGas:0,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  }
+};
+
+
+export default config;
